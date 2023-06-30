f33a2f14d7de6c776214c92f4258fffb043738e0
Gowsikan <gowsikan32412@gmail.com>
2023-06-30 10:16:45 +0530

    nav bar menus added

--- cr-ar-app/src/Components/NavBar.js
+++ cr-ar-app/src/Components/NavBar.js
@@ -1,16 +1,23 @@
-import "../Styles/main.css";
+import "../Styles/NavBarStyles.css";
 import { Box } from "./Box";
+import { MenuData } from "./MenuData";
 
 
 function NavBar() {
     return (  
-        <header>
-            <Box>
-                <label>
-                    AR CR App
-                </label>
-            </Box>
-        </header>
+        <nav className="NavbarItems">
+            <h1 className="logo">
+                CR AR App
+            </h1>
+            <ul className="nav-menu">
+                {MenuData.map((item,index)=>{
+                    return(
+                        <li key={index}><a href={item.url} className={item.cName}>
+                            {item.title}
+                        </a></li>
+                    )})}
+            </ul>
+        </nav>
     );
 }
 
