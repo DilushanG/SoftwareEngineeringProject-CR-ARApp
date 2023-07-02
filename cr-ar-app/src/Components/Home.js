b46baedd143942d64c0d7c741851b478f6361ae8
Gowsikan <gowsikan32412@gmail.com>
2023-07-02 22:58:57 +0530

    Home page added

---
+++ cr-ar-app/src/Components/Home.js
@@ -0,0 +1,32 @@
+import "../Styles/HomeStyles.css";
+
+function Home() {
+    return (<>
+    <div className="home">
+        <div className="front">
+        <div className="box">
+      <div className="h-wrapper">
+        <div className="h">
+          <div className="overlap-group">
+            <h1 className="the-expert-in">
+              <span className="text-wrapper">The Expert, </span>
+              <span className="span">in Anything</span>
+            </h1>
+            <div className="was-once-a-beginner">
+              <span className="span">was once</span>
+              <span className="text-wrapper-2">&nbsp;</span>
+              <span className="text-wrapper">A Beginner.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+        <div className="topBox">
+            Welcome GowsiKan
+        </div>
+    </div>
+    </>);
+}
+
+export default Home;
