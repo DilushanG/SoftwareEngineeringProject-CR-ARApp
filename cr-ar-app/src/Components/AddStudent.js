ee3cc3ed7b6d5c60813374fd6cca80e40c9a3e3e
Jathurshan <sjathurshan19980419@gmail.com>
2023-07-09 03:39:21 +0530

    addstudent page

---
+++ cr-ar-app/src/Components/AddStudent.js
@@ -0,0 +1,12 @@
+import "../Styles/AddStudentStyles.css";
+function AddStudent() {
+    return ( <div className="student">
+        <h1>
+        <div className="head">
+        <span className="text">student </span>
+        </div>
+        </h1>
+        </div> );
+}
+
+export default AddStudent ;
