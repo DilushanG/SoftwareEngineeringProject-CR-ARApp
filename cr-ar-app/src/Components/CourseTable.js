e67012fecffe8fad3cbedafff33c23810608512c
Aarthy <2019e001@eng.jfn.ac.lk>
2023-07-12 03:03:46 +0530

    edit course table details

--- cr-ar-app/src/Components/CourseTable.js
+++ cr-ar-app/src/Components/CourseTable.js
@@ -1,8 +1,17 @@
+import "../Styles/StudentStyle.css";
+import SearchBar from './SearchBar';
+
+import DropDownYear from "./DropDownYear";
+import React, { useState } from 'react';
+import "../Styles/main.css"
 import "../Styles/HomeStyles.css";
-import "../Images/arrow.png";
-//import DropDownYear from "./DropDownYear";
-import "../Styles/CourseTable.css";
-import Table from "./Table.js";
+import Table from "./Table";
+
+
+
+import DropDownDepartment from "./DropDownDepartment";
+import DropDownSemester from "./DropDownSemester";
+import DownloadButton from "./DownloadButton";
 
 const list = [
     { Code: "EC1010",Course: "Software Construction",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System",Status: "Close"},
@@ -17,10 +26,48 @@ const list = [
   const colNames = ['Code','Course','Credit','Core/Technical','Coordinator','Prerequiste','Status'];
   
   function CourseTable() {
+    
+    const handleSearch = (searchTerm) => {
+      // Perform search logic here using the searchTerm
+      console.log('Search term:', searchTerm);
+    };
+    const handledownload = (Term) => {
+      // Perform search logic here using the searchTerm
+      console.log('Download:', Term);
+    };
+    
     return (
+      
       <div>
+        <div className="box">
+        Course
+        <div className="SearchBar">
+        <SearchBar onSearch={handleSearch} />
+        <div className="box">
+        <div className="box-2">
+        <div className="h-2"><DropDownYear/>
+        <DropDownDepartment/>
+        <DropDownSemester/></div>
+
+        <div>
         <Table list={list} colNames={colNames}/>
+        </div>
+
+        <div>
+          <DownloadButton onClick={handledownload} />
+        </div>
+
+        </div>
+        </div>
+        </div>
+        </div>
+      
       </div>
+      
+      
+        
+
+      
     );
   }
   
